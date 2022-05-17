3946877eebea3cb6aaeae851d8b410fa6f2d7848
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-05-17 10:06:20 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Hello">
+      <h1>Hello World !</h1>
+      <p>This is my React JS Code</p>
+    </div>
+  );
+}
+
+export default App;
